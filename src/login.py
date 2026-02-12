205e83dd675344ccac02b0f2f58e82d14d2bbac7
emanuell.rossetto <emanuell.rossetto@ecocentauro.com.br>
2026-02-12 18:01:08 -0300

    Atualizando

--- src/login.py
+++ src/login.py
@@ -1,3 +1,8 @@
+"""
+CONTEXTO PARA O GITHUB COPILOT:
+Estou usando Flet versão 0.80.5.
+"""
+
 import flet as ft
 
 def main(page: ft.Page):
@@ -8,22 +13,37 @@ def main(page: ft.Page):
             page.update()
     page.on_keyboard_event = on_keyboard
 
-    page.add(
-       ft.Stack(
-        expand=True,
-        controls=[
-            ft.Container(
-                width=200,
-                height=100,
-                bgcolor=ft.Colors.RED,
-                content=ft.Text("Topo direita", text_align=ft.TextAlign.CENTER),
-                top=20,
-                right=30,
-            )
-        ],
-    )
-        
-    )
+    
+    textTitulo = ft.Text("Login", size=30, weight=ft.FontWeight.BOLD, align=ft.Alignment.CENTER)
+    textLogin = ft.TextField(label="Login", width=200)
+    textSenha = ft.TextField(label="Senha", width=200, password=True)
 
-  
+    # btnEntrar = ft.ElevatedButton(text="Entrar", width=200)
+    teste=ft.Container(
+        bgcolor=ft.Colors.BLUE,
+        content=ft.Column(controls=[
+            textLogin,
+            textSenha,
+            
+
+        ])
+    )
+    
+    page.add(
+       ft.Container(
+           expand=True,
+            content= ft.Column(
+                controls=[
+                    textTitulo,
+                    teste
+                ],
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                spacing=20
+            ),
+            bgcolor=ft.Colors.WHITE,
+            align=ft.Alignment.CENTER,
+            width=400, #LADOS
+            height=600, #CIMA
+        )
+    )      
 ft.run(main)
