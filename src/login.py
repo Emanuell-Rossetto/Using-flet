fa6bcdb2eff836019eed601ec0d35386a1861d95
Emanuell-Rossetto <emanuellrossetto@hotmail.com>
2026-02-13 00:15:23 -0300

    atualizando

--- src/login.py
+++ src/login.py
@@ -6,7 +6,8 @@ Estou usando Flet versão 0.80.5.
 import flet as ft
 
 def main(page: ft.Page):
-
+    background = "#121212"
+    loginBackground = "#1E1E1E"
     def on_keyboard(e: ft.KeyboardEvent):
         if e.shift and e.key == "S":           
             page.show_semantics_debugger = not page.show_semantics_debugger          
@@ -14,18 +15,21 @@ def main(page: ft.Page):
     page.on_keyboard_event = on_keyboard
 
     
-    textTitulo = ft.Text("Login", size=30, weight=ft.FontWeight.BOLD, align=ft.Alignment.CENTER)
+    textTitulo = ft.Text("🐀 R.A.T.O", size=30, weight=ft.FontWeight.BOLD, margin=ft.Margin(0, 0, 0, 20))
     textLogin = ft.TextField(label="Login", width=200)
     textSenha = ft.TextField(label="Senha", width=200, password=True)
 
     # btnEntrar = ft.ElevatedButton(text="Entrar", width=200)
     teste=ft.Container(
-        bgcolor=ft.Colors.BLUE,
-        content=ft.Column(controls=[
+        
+        align=ft.Alignment.CENTER, #Centraliza o container INTERNO
+        bgcolor=loginBackground,
+        content=ft.Column(
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER, #Centraliza os itens dentro da coluna
+            controls=[
+            textTitulo,
             textLogin,
             textSenha,
-            
-
         ])
     )
     
@@ -33,15 +37,14 @@ def main(page: ft.Page):
        ft.Container(
            expand=True,
             content= ft.Column(
+                alignment=ft.MainAxisAlignment.CENTER,
                 controls=[
-                    textTitulo,
                     teste
                 ],
-                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-                spacing=20
+                spacing=20,
             ),
-            bgcolor=ft.Colors.WHITE,
-            align=ft.Alignment.CENTER,
+            bgcolor=background,
+            align=ft.Alignment.CENTER, #Centraliza o container CENTRAL
             width=400, #LADOS
             height=600, #CIMA
         )
